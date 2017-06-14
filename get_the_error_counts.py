ae7bc30839490db47ed4bc6e165063db9c6d6bc9
LiCheng <bbbbb34016@gmail.com>
2017-06-14 10:55:19 +0800

    Update get_the_error_counts.py

--- get_the_error_counts.py
+++ get_the_error_counts.py
@@ -14,6 +14,7 @@ month_dict = {'1':'Jan','2':'Feb','3':'Mar','4':'Apr','5':'May','6':'Jun','7':'J
 path = os.path.dirname(os.path.abspath(__file__)) #get file path
 create_time = str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')) #create time
 dcn_files=glob.glob(path+'\*.dcn') #get all .dcn file
+log_files=glob.glob(path+'\*.log')  #get all .log file
 csvfile = open(month_dict[month_number]+'_errors'+create_time+'.csv','a',newline='')
 no_error_file = open(month_dict[month_number]+'_no_errors'+create_time+'.csv','a',newline='')
 csvwriter = csv.writer(csvfile)
@@ -25,68 +26,17 @@ total_dict = {} #total count dictionary
 ipname_error_dict = {} #ipname_errorname_errorcount format dictionary
 ranking_dict = {} #dictionary for ranking
 
-for file_name in dcn_files:
-    ip_file = file_name.split('\\')[-1]
-       
-    test_file = open(file_name,'r',encoding='utf-8') #utf-8 encoding for .dcn files
-    lines_file = test_file.readlines()
-    
-    error_dict = {} #error_count format dictionary
-    index_line = 0  #the index of 'Log Buffer' line
-    error_count = 0 #the count of error 
-    
 
-    
+
+#count error
+def count_error(test_file):
+    lines_file = test_file.readlines()       
+
     for i in range(len(lines_file)):
-        if 'Log Buffer' in lines_file[i]:   #the error information is under the 'Log Buffer'
-            index_line = i+1
-            break
-        
-    while index_line>0:
-        error_info = lines_file[index_line].split(': ')        #'time:errorname:cause' is the format of error in .dcn 
-        if 'show run' in lines_file[index_line]:            #the error information is above on the 'show run'
-            break
-        if len(error_info)>2 and month_dict[month_number] in error_info[0]:
-            error_name = error_info[1]          #error_info='time:errorname:cause'
-            if error_name not in error_dict:
-                error_dict[error_name]=1                
-            else:
-                error_dict[error_name]+=1                
-            if error_name not in total_dict:                
-                total_dict[error_name]=1
-            else:               
-                total_dict[error_name]+=1
-            ipname_error_dict[ip_file] =error_dict
-        index_line += 1        
-    index_line = 0
-
-    if error_dict:
-        csvwriter.writerow([ip_file])   
-        for error_name,error_count in error_dict.items():               
-            csvwriter.writerow(['',error_name,error_count])
-    else:
-        no_error_writer.writerow([ip_file])
-        
-log_files=glob.glob(path+'\*.log')  #get all .log file
-
-
-for file_name in log_files:
-    
-    ip_file = file_name.split('\\')[-1]
-    csvwriter.writerow([ip_file])    
-    test_file = open(file_name,'r')
-    lines_file = test_file.readlines()
-    
-    error_dict = {} #error_count format dictionary 
-    index_line = 0 #the index of 'Log Buffer' line
-    error_count = 0 #the count of error
-    
-    
-    for i in range(len(lines_file)):
-        if 'Log Buffer' in lines_file[i]:       #the error information is under the 'Log Buffer'
-            index_line = i+1
-            break
-        
+            if 'Log Buffer' in lines_file[i]:       #the error information is under the 'Log Buffer'
+                index_line = i+1
+                break
+            
     while index_line>0:
         error_info = lines_file[index_line].split(': ')        #'time:errorname:cause' is the format of error in .log 
         if 'show run' in lines_file[index_line]:            #the error information is above on the 'show run'
@@ -103,21 +53,72 @@ for file_name in log_files:
                 total_dict[error_name]+=1
             ipname_error_dict[ip_file] =error_dict
         index_line += 1        
-    index_line = 0    
-    
+    index_line = 0
+
+
+#write error
+def write_error():    
     if error_dict:
         csvwriter.writerow([ip_file])   
         for error_name,error_count in error_dict.items():               
             csvwriter.writerow(['',error_name,error_count])
     else:
         no_error_writer.writerow([ip_file])
+        
+
+#write total error count
+def write_total_error_count(total_dict):
+    for total_error_name,total_error_count in total_dict.items():    
+        csvwriter.writerow(['',total_error_name,total_error_count])
+        
+        
+#write ranking error
+def write_ranking_error(ranking_dict):
+    for error_name,ip_count_dict in ranking_dict.items():
+        ip_count_dict = sorted(ip_count_dict.items(),key=lambda x:x[1],reverse=True) #sort the error with counts
+        times = 1 #ranking index
+        csvwriter.writerow(['Ranking',error_name,''])
+    
+        for ip_name,error_count in ip_count_dict:
+            if len(ip_count_dict)<11:   #display all ranking if not larger than 10
+                csvwriter.writerow([times,ip_name,error_count])
+                times += 1
+            else:
+                if times<11:    #display top ten ranking
+                    csvwriter.writerow([times,ip_name,error_count])
+                    times += 1
+      
+
+
+    
+#main function
+for file_name in dcn_files:
+    ip_file = file_name.split('\\')[-1]
+    with open(file_name,'r',encoding='utf-8') as test_file:   
+        #test_file = open(file_name,'r',encoding='utf-8') #utf-8 encoding for .dcn files
+        error_dict = {} #error_count format dictionary 
+        index_line = 0 #the index of 'Log Buffer' line
+        error_count = 0 #the count of error
+        count_error(test_file)
+        write_error()
+
+for file_name in log_files:    
+    ip_file = file_name.split('\\')[-1]
+    csvwriter.writerow([ip_file])
+    with open(file_name,'r') as test_file:
+        #test_file = open(file_name,'r')
+        
+        
+        error_dict = {} #error_count format dictionary 
+        index_line = 0 #the index of 'Log Buffer' line
+        error_count = 0 #the count of error
+        count_error(test_file)
+        write_error()        
 
 
 #display total counts for each error
 csvwriter.writerow(['Total errors'])
-
-for total_error_name,total_error_count in total_dict.items():    
-    csvwriter.writerow(['',total_error_name,total_error_count])
+write_total_error_count(total_dict)
     
 
 for ip_name,error_dict in ipname_error_dict.items():
@@ -134,18 +135,8 @@ for ip_name,error_dict in ipname_error_dict.items():
 
 #display top ten error ip for each error
 csvwriter.writerow(['Error ranking'])
+write_ranking_error(ranking_dict)
 
 
-for error_name,ip_count_dict in ranking_dict.items():
-    ip_count_dict = sorted(ip_count_dict.items(),key=lambda x:x[1],reverse=True) #sort the error with counts
-    times = 1 #ranking index
-    csvwriter.writerow(['Ranking',error_name,''])
     
-    for ip_name,error_count in ip_count_dict:
-        if len(ip_count_dict)<11:   #display all ranking if not larger than 10
-            csvwriter.writerow([times,ip_name,error_count])
-            times += 1
-        else:
-            if times<11:    #display top ten ranking
-                csvwriter.writerow([times,ip_name,error_count])
-                times += 1
+    
